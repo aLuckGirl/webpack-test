8678cfeb694c9f697a642968ae08d0171374072d
张明明 <1039404127@qq.com>
2019-09-26 14:39:42 +0800

    新增：demo1，简单的打包

---
+++ demo1/src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = 'hahahah'
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' '); //要用到lodash的语法
+
+    return element;
+}
+document.body.appendChild(component());
